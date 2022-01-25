86029ffb86545e254f074cf4b7413934e72c13ef
Tatyana <Tatyana@DESKTOP-74DNP3C>
2022-01-25 20:22:58 +0300

    Добавьте файлы проекта.

---
+++ Labb15/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb15
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arith = new ArithProgression();
+            arith.setStart(2);
+            arith.setStep(10);
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            arith.reset();
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.ReadKey();
+            Console.WriteLine();
+
+            GeomProgression geom = new GeomProgression();
+            geom.setStart(10);
+            geom.setStep(10);
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            geom.reset();
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.ReadKey();
+        }
+
+    }
+}
